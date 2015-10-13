9767e4c4b84d1cb1af7f24a9fa4fa4c1628ee4ef
Devin Nusbaum <d.w.nusbaum@gmail.com>
2015-10-13 15:03:22 -0400

    Add challenge 2

---
+++ set1/Challenge2.hs
@@ -0,0 +1,26 @@
+module Challenge1 (
+    main
+) where
+
+import Data.Bits
+
+import qualified Data.ByteString as B
+import qualified Data.ByteString.Char8 as C
+
+import Hex
+import Base64
+
+hexstring1 :: String
+hexstring1 = "1c0111001f010100061a024b53535009181c"
+
+hexstring2 :: String
+hexstring2 = "686974207468652062756c6c277320657965"
+
+expectedHex :: String
+expectedHex = "746865206b696420646f6e277420706c6179"
+
+main :: IO ()
+main = print $ result == expectedHex
+  where bytes1 = B.unpack $ C.pack $ decodeHex hexstring1
+        bytes2 = B.unpack $ C.pack $ decodeHex hexstring2
+        result = encodeHex $ C.unpack $ B.pack $ zipWith xor bytes1 bytes2
