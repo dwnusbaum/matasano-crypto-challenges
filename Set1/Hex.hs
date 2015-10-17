4cd80d3c91433d2540e9ef7e3e5ee9d5183a7e07
Devin Nusbaum <d.w.nusbaum@gmail.com>
2015-10-17 15:25:27 -0400

    Refactor Hex.hs to export showHex

--- Set1/Hex.hs
+++ Set1/Hex.hs
@@ -3,24 +3,21 @@
 module Hex (
     encodeHex,
     decodeHex,
+    showHex
 ) where
 
 import Data.Bits
-import Data.Maybe (fromJust)
 import Data.Word(Word8)
 
 import qualified Data.ByteString as B
 import qualified Data.ByteString.Char8 as C
+import qualified Data.Vector as V
 
 hexAlphabet :: String
 hexAlphabet = ['0'..'9'] ++ ['a'..'f']
 
 encodeHex :: String -> String
-encodeHex = concatMap word8ToHexString . B.unpack . C.pack
-  where word8ToHexString w = [word8ToHexChar upperHalf, word8ToHexChar lowerHalf]
-          where lowerHalf = w .&. 0x0F
-                upperHalf = (w .&. 0xF0) `shiftR` 4
-                word8ToHexChar x = fromJust $ lookup x $ zip [0..15] hexAlphabet
+encodeHex = concatMap showHex . B.unpack . C.pack
 
 decodeHex :: String -> String
 decodeHex = C.unpack . B.pack . hexStringToWord8List
@@ -31,3 +28,9 @@ decodeHex = C.unpack . B.pack . hexStringToWord8List
                     Just n -> fromInteger n
                     Nothing -> error $ "hexCharToWord8: Invalid character \"" ++ [c] ++ "\""
         hexStringToWord8List _ = error "Invalid hex string."
+
+showHex :: Word8 -> String
+showHex b = [alphabet V.! upperHalf b, alphabet V.! lowerHalf b]
+  where upperHalf = fromIntegral . (`shiftR` 4)
+        lowerHalf = fromIntegral . (.&. 0x0F)
+        alphabet = V.fromList $ ['0'..'9'] ++ ['a'..'f']
