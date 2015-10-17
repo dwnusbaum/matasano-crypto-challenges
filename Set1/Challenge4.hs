a6f202f3bc4ccce26f982b914ba964d7faa17131
Devin Nusbaum <d.w.nusbaum@gmail.com>
2015-10-17 15:30:38 -0400

    Change function name to improve clarity

--- Set1/Challenge4.hs
+++ Set1/Challenge4.hs
@@ -11,11 +11,11 @@ import Challenge3 hiding (main)
 
 import Hex
 
-findSingleByteXor :: [String] -> (String, Char)
-findSingleByteXor xs = minimumBy (comparing (naturalLanguageScore . fst)) output
+detectSingleByteXor :: [String] -> (String, Char)
+detectSingleByteXor xs = minimumBy (comparing (naturalLanguageScore . fst)) output
   where output = map (crackSingleByteXor . decodeHex) xs
 
 main :: IO ()
 main = do
     file <- readFile "data/4.txt"
-    print $ findSingleByteXor $ lines file
+    print $ detectSingleByteXor $ lines file
