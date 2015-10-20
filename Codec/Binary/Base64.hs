37f8a20751b3c31affedb376a51b07719fa47379
Devin Nusbaum <d.w.nusbaum@gmail.com>
2015-10-19 20:23:28 -0400

    Fix bug when decoding base64

--- Codec/Binary/Base64.hs
+++ Codec/Binary/Base64.hs
@@ -37,8 +37,8 @@ decodeBase64 = C.unpack . B.pack . base64StringToWord8List . takeWhile (/= '=')
   where base64StringToWord8List [] = []
         base64StringToWord8List ws = case length ws of
             1 -> error "Base64.hs: Not possible"
-            2 -> take 2 $ decodeQuartet $ map base64CharToWord8 $ ws ++ "AA"
-            3 -> take 3 $ decodeQuartet $ map base64CharToWord8 $ ws ++ "A"
+            2 -> take 1 $ decodeQuartet $ map base64CharToWord8 $ ws ++ "AA"
+            3 -> take 2 $ decodeQuartet $ map base64CharToWord8 $ ws ++ "A"
             _ -> decodeQuartet (map base64CharToWord8 $ take 4 ws) ++ base64StringToWord8List (drop 4 ws)
           where base64CharToWord8 x = fromMaybe (error $ show x) (lookup x $ zip base64Alphabet [0..63])
 
