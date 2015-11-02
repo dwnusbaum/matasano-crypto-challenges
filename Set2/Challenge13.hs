ca7d09dad4952cb5fd87b096de0383a4334d4b71
Devin Nusbaum <d.w.nusbaum@gmail.com>
2015-11-02 13:31:27 -0500

    Remove unused imports and clean up output

--- Set2/Challenge13.hs
+++ Set2/Challenge13.hs
@@ -2,14 +2,12 @@ module Set2.Challenge13 (
     main
 ) where
 
-import qualified Data.ByteString as B
-import qualified Data.ByteString.Char8 as C
-
-import Crypto.AES
 import Crypto.QueryString.Attack
 import Crypto.QueryString.Utils
 
 main :: IO ()
 main = do
     let adminBytes = buildAdminProfile profileFor
-    print $ decryptQueryString adminBytes
+    case decryptQueryString adminBytes of
+        Left err -> print err
+        Right profile -> print profile
